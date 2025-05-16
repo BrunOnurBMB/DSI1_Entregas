52f55183c61ac7988f4879fae08fad742adff30d
BrunOnurBMB <bruno.silveira.0803@gmail.com>
2025-05-15 22:30:49 -0300

    Subindo programa funcionando

--- Projeto Login/src/login/TelaAlteracao.java
+++ Projeto Login/src/login/TelaAlteracao.java
@@ -105,6 +105,7 @@ public class TelaAlteracao extends JFrame {
 				usu.setSenha(confPassSenha.getText());
 				usu.setUsuario(usuarioSistema);
 				
+				
 				//Nome Vazio
 				if("".equals(usu.getNome())) {
 					//Mensagem em tela
@@ -154,6 +155,10 @@ public class TelaAlteracao extends JFrame {
 						//Usuario cadastrado na base da daos
 						JOptionPane.showMessageDialog(null, "Problemas ao atualizar o usuário", 
 															"Atenção", JOptionPane.ERROR_MESSAGE);
+						System.out.println(usu.getNome());
+						System.out.println(usu.getSenha());
+						System.out.println(usu.verificaUsuario(usu.getUsuario()));
+						System.out.println(passSenha.getText().equals(confPassSenha.getText()));
 					}
 				}
 			} catch (HeadlessException ec) {
